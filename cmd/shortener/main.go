5fa3a75d6f10b54f6548ba11e382a955eacaa65d
Bauner <bauner@yandex.ru>
2022-10-22 08:56:12 +0300

    main file

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"go-shortener-tpl/internal/app"
+	"log"
+	"net/http"
+)
+
+var LocalDB []app.DB
+
+func main() {
+
+	http.HandleFunc("/", app.HandlerRequest)
+
+	// start server
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
