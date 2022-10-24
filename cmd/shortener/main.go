d0e27b22e83494be4757d1bf202d755dad6596ad
Bauner <bauner@yandex.ru>
2022-10-24 20:12:14 +0300

    need help v2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-
 	http.HandleFunc("/", app.HandlerRequest)
 	// start server
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -1,16 +1,15 @@
 package app
 
 import (
-	"encoding/base64"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
-	"time"
 )
 
 func HandlerRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Method)
 	switch r.Method {
 	case "POST":
 		URL, err := io.ReadAll(r.Body)
@@ -18,13 +17,13 @@ func HandlerRequest(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		timestamp := time.Now()
-		shr := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s %d", string(URL)[2:6], timestamp.Second())))
-		resStruct := DB{len(LocalDB), string(URL), shr}
+		hash := md5.New()
+		shr := hash.Sum(URL)
+		resStruct := DB{len(LocalDB), string(URL), hex.EncodeToString(shr)}
 		SaveDB(resStruct)
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(fmt.Sprintf("http://localhost:8080/%s", shr)))
+		w.Write([]byte(fmt.Sprintf("http://localhost:8080/%s", hex.EncodeToString(shr))))
 	case "GET":
 		id := strings.TrimPrefix(r.RequestURI, "/")
 		columns, err := FindByID(id)
@@ -33,9 +32,9 @@ func HandlerRequest(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		w.Header().Set("Content-Type", "text/plain")
+		fmt.Println(columns.URL)
 		w.Header().Set("Location", columns.URL)
 		w.WriteHeader(http.StatusTemporaryRedirect)
-		w.Write([]byte(fmt.Sprintf("http://localhosy:8080/%s", columns.URLShort)))
 	default:
 		http.Error(w, "Bad Gateway", http.StatusMethodNotAllowed)
 		return
