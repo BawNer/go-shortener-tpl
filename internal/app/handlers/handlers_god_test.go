a0a399ceb9440dc22662338f2d99bc9905dd68d8
Bauner <bauner@yandex.ru>
2022-10-31 17:32:09 +0300

    add test endpoint

---
+++ internal/app/handlers/handlers_god_test.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+)
+
+func TestMemStorage_HandlerRequest(t *testing.T) {
+	test := []struct {
+		name       string
+		method     string
+		wantStatus string
+		wantBody   bool
+	}{
+		{
+			name:       "Test POST Method",
+			method:     http.MethodPost,
+			wantStatus: "201 Created",
+			wantBody:   true,
+		},
+		{
+			name:       "Test GET Method",
+			method:     http.MethodGet,
+			wantStatus: "404 Not Found",
+			wantBody:   true,
+		},
+	}
+
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			request, err := http.NewRequest(tt.method, "http://localhost:8080/", nil)
+			if err != nil {
+				t.Errorf("Expect error from request!")
+			}
+			resp, err := http.DefaultClient.Do(request)
+			defer resp.Body.Close()
+			payload, err := io.ReadAll(resp.Body)
+			if resp.Status != tt.wantStatus {
+				t.Errorf("Await status %s, but get status %s", tt.wantStatus, resp.Status)
+			}
+			if tt.wantBody && resp.Body == nil {
+				t.Errorf("Body is empty, but await body")
+			}
+			fmt.Println(payload)
+		})
+	}
+}
