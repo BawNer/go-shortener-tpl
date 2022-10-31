fd6ba391045157474e9fc3508a6e2da821257b86
Bauner <bauner@yandex.ru>
2022-10-31 17:58:44 +0300

    refactor tests

--- internal/app/handlers/handlers_god_test.go
+++ internal/app/handlers/handlers_god_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -10,43 +11,43 @@ func TestMemStorage_HandlerRequest(t *testing.T) {
 	test := []struct {
 		name       string
 		method     string
-		wantStatus string
+		wantStatus int
 		wantBody   bool
 	}{
 		{
 			name:       "Test POST Method",
 			method:     http.MethodPost,
-			wantStatus: "201 Created",
+			wantStatus: 201,
 			wantBody:   true,
 		},
 		{
 			name:       "Test GET Method",
 			method:     http.MethodGet,
-			wantStatus: "404 Not Found",
+			wantStatus: 404,
 			wantBody:   true,
 		},
 	}
 
 	for _, tt := range test {
 		t.Run(tt.name, func(t *testing.T) {
-			request, err := http.NewRequest(tt.method, "http://localhost:8080/", nil)
+			sh := &MemStorage{}
+			request := httptest.NewRequest(tt.method, "http://localhost:8080", nil)
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(sh.HandlerRequest)
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			if res.StatusCode != tt.wantStatus {
+				t.Errorf("Expected status code %d, got %d", tt.wantStatus, w.Code)
+			}
+
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
 			if err != nil {
-				t.Errorf("Expect error from request!")
+				t.Fatal(err)
 			}
-			resp, err := http.DefaultClient.Do(request)
-			if err != nil {
-				t.Errorf("Expect error from request!")
-			}
-			defer resp.Body.Close()
-			payload, err := io.ReadAll(resp.Body)
-			if err != nil {
-				t.Errorf("Expect error from read body!")
-			}
-			if resp.Status != tt.wantStatus {
-				t.Errorf("Await status %s, but get status %s", tt.wantStatus, resp.Status)
-			}
-			if tt.wantBody && payload == nil {
-				t.Errorf("Body is empty, but await body")
+			if !tt.wantBody && resBody != nil {
+				t.Errorf("Expect body!!!")
 			}
 		})
 	}
