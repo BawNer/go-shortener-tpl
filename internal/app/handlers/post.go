019d1bf1b4041c88c41760243a8862248e15b6f0
Bauner <bauner@yandex.ru>
2022-11-03 19:21:17 +0300

    replace project, update modules, add more space between actions

--- internal/app/handlers/post.go
+++ internal/app/handlers/post.go
@@ -3,22 +3,35 @@ package handlers
 import (
 	"encoding/hex"
 	"fmt"
-	"github.com/google/uuid"
-	"go-shortener-tpl/internal/app/storage"
 	"io"
 	"net/http"
+
+	"github.com/BawNer/go-shortener-tpl/internal/app/storage"
+	"github.com/google/uuid"
 )
 
 func (m *MemStorage) HandlerPostRequest(w http.ResponseWriter, r *http.Request) {
 	URL, err := io.ReadAll(r.Body)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	shr := uuid.New().NodeID()
 	URLShort := hex.EncodeToString(shr)
-	m.SaveDB(storage.DBKey(URLShort), storage.MyDB{ID: len(m.Storage), URL: string(URL), URLShort: URLShort})
+
+	m.SaveDB(
+		storage.DBKey(URLShort),
+		storage.MyDB{
+			ID:       len(m.Storage),
+			URL:      string(URL),
+			URLShort: URLShort,
+		})
+
 	w.Header().Set("Content-Type", "text/plain")
+
 	w.WriteHeader(http.StatusCreated)
+
 	_, _ = w.Write([]byte(fmt.Sprintf("http://localhost:8080/%s", URLShort)))
 }
