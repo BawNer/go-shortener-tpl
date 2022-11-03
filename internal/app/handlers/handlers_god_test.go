01bbf01663f114cba452f13518cd36e454979679
Bauner <bauner@yandex.ru>
2022-11-03 19:20:39 +0300

    replace project, update modules

--- internal/app/handlers/handlers_god_test.go
+++ internal/app/handlers/handlers_god_test.go
@@ -1,11 +1,12 @@
 package handlers
 
 import (
-	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func TestMemStorage_HandlerRequest(t *testing.T) {
