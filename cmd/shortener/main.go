3a763be55447662b695189d2fce39d15830753ce
Bauner <bauner@yandex.ru>
2022-11-11 21:25:46 +0300

    refactor code

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"http://localhost:8080"`
 	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 }
 
