33265116fdfc17890e98fc24eaff2bf4b8b9a8ab
Bauner <bauner@yandex.ru>
2022-11-14 20:46:34 +0300

    fix default post req

--- internal/app/handlers/post.go
+++ internal/app/handlers/post.go
@@ -5,13 +5,20 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 
+	"github.com/BawNer/go-shortener-tpl/internal/app"
 	"github.com/BawNer/go-shortener-tpl/internal/app/storage"
 	"github.com/google/uuid"
 )
 
 func (m *MemStorage) HandlerPostRequest(w http.ResponseWriter, r *http.Request) {
 	URL, err := io.ReadAll(r.Body)
+
+	cfg := app.NewConfig(&app.Config{
+		ServerAddr: os.Getenv("SERVER_ADDRESS"),
+		BaseURL:    os.Getenv("BASE_URL"),
+	})
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -33,5 +40,5 @@ func (m *MemStorage) HandlerPostRequest(w http.ResponseWriter, r *http.Request) 
 
 	w.WriteHeader(http.StatusCreated)
 
-	_, _ = w.Write([]byte(fmt.Sprintf("http://localhost:8080/%s", URLShort)))
+	_, _ = w.Write([]byte(fmt.Sprintf("%s/%s", cfg.BaseURL, URLShort)))
 }
