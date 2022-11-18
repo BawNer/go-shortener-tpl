553f8b5c29266e8f42a61e77e9207ed50f1381c0
Bauner <bauner@yandex.ru>
2022-11-18 22:47:30 +0300

    refactor consumer.go

--- internal/app/storage/consumer.go
+++ internal/app/storage/consumer.go
@@ -3,39 +3,50 @@ package storage
 import (
 	"bufio"
 	"encoding/json"
+	"log"
 	"os"
 )
 
 type consumer struct {
 	file    *os.File
+	decoder *json.Decoder
 	scanner *bufio.Scanner
 }
 
-func NewConsumer(filename string) (*consumer, error) {
-	file, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0777)
+func NewConsumer(fileName string) (*consumer, error) {
+	file, err := os.OpenFile(fileName, os.O_RDONLY|os.O_CREATE, 0777)
 	if err != nil {
 		return nil, err
 	}
-
 	return &consumer{
 		file:    file,
+		decoder: json.NewDecoder(file),
 		scanner: bufio.NewScanner(file),
 	}, nil
 }
 
-func (c *consumer) ReadEvent() (*Event, error) {
-	if !c.scanner.Scan() {
-		return nil, c.scanner.Err()
-	}
-	data := c.scanner.Bytes()
-
-	event := Event{}
-	err := json.Unmarshal(data, &event)
-	if err != nil {
+func (c *consumer) ReadEvent() (*MyDB, error) {
+	event := &MyDB{}
+	if err := c.decoder.Decode(&event); err != nil {
 		return nil, err
 	}
+	return event, nil
+}
 
-	return &event, nil
+func (c *consumer) ReadEventAll() ([]MyDB, error) {
+	var (
+		event    MyDB
+		eventAll []MyDB
+	)
+	for c.scanner.Scan() {
+		data := c.scanner.Bytes()
+		if err := json.Unmarshal(data, &event); err != nil {
+			log.Fatal(err)
+		}
+		eventAll = append(eventAll, event)
+	}
+
+	return eventAll, nil
 }
 
 func (c *consumer) Close() error {
