1b9f3bdc82c1fd39ad35a0c92f6c19a1bbf44c1e
Bauner <bauner@yandex.ru>
2022-11-18 22:47:39 +0300

    refactor producer.go

--- internal/app/storage/producer.go
+++ internal/app/storage/producer.go
@@ -1,49 +1,33 @@
 package storage
 
 import (
-	"bufio"
 	"encoding/json"
 	"os"
 	"path/filepath"
 )
 
 type producer struct {
-	file   *os.File
-	writer *bufio.Writer
+	file    *os.File
+	encoder *json.Encoder
 }
 
-func NewProducer(filename string) (*producer, error) {
-	if _, err := os.Stat(filepath.Dir(filename)); err != nil {
-		os.MkdirAll(filepath.Dir(filename), 0770)
+func NewProducer(fileName string) (*producer, error) {
+	if _, err := os.Stat(filepath.Dir(fileName)); err != nil {
+		os.MkdirAll(filepath.Dir(fileName), 0770)
 	}
-	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
 	if err != nil {
 		return nil, err
 	}
-
 	return &producer{
-		file:   file,
-		writer: bufio.NewWriter(file),
+		file:    file,
+		encoder: json.NewEncoder(file),
 	}, nil
 }
-
-func (p *producer) WriteEvent(event *Event) error {
-	data, err := json.Marshal(&event)
-	if err != nil {
-		return err
-	}
-
-	if _, err := p.writer.Write(data); err != nil {
-		return err
-	}
-
-	if err := p.writer.WriteByte('\n'); err != nil {
-		return err
-	}
-
-	return p.writer.Flush()
+func (p *producer) WriteEvent(event *MyDB) error {
+	p.encoder.SetEscapeHTML(false)
+	return p.encoder.Encode(&event)
 }
-
 func (p *producer) Close() error {
 	return p.file.Close()
 }
