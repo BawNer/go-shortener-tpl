791224ad8a1fba7840cc9f5273d9a2a232149719
Bauner <bauner@yandex.ru>
2022-11-20 13:02:54 +0300

    fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/BawNer/go-shortener-tpl/internal/app"
 	"github.com/BawNer/go-shortener-tpl/internal/app/handlers"
+	"github.com/BawNer/go-shortener-tpl/internal/middlewares"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -22,7 +23,7 @@ func main() {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.Compress(5))
+	r.Use(middlewares.GzipHandle)
 
 	r.Post("/api/shorten", h.ShortenerHandler)
 	r.Post("/", h.HandlerPostRequest)
