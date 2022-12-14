052432776da5415700a3a6c176a45cb5679698c1
Bauner <bauner@yandex.ru>
2022-12-14 20:23:44 +0300

    refactor

--- internal/app/handlers/delete_batch.go
+++ internal/app/handlers/delete_batch.go
@@ -44,7 +44,7 @@ func (h *Handler) HandleDeleteBatchUrls(w http.ResponseWriter, r *http.Request) 
 	}
 
 	// main
-	batchSize = len(urlIDs)
+	batchSize = len(urlIDs) + 2
 	inputCh := make(chan dataForWorker, batchSize)
 
 	go putJobs(inputCh, urlIDs, signID)
