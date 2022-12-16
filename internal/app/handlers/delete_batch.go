dc9445374fdb360fc2ce98295da3f2f5f5151cde
Bauner <bauner@yandex.ru>
2022-12-16 10:51:30 +0300

    add more logs

--- internal/app/handlers/delete_batch.go
+++ internal/app/handlers/delete_batch.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/BawNer/go-shortener-tpl/internal/app"
 	"github.com/BawNer/go-shortener-tpl/internal/app/storage"
+	"github.com/google/uuid"
 )
 
 type dataForWorker struct {
@@ -21,20 +22,30 @@ func (h *Handler) HandleDeleteBatchUrls(w http.ResponseWriter, r *http.Request) 
 		signID uint32
 		urlIDs []string
 	)
+
+	reqID := uuid.New().String()
+	log.Printf("reqID=%s handle request HandleDeleteBatchUrls", reqID)
+
+	log.Printf("reqID=%s Получаем id's из запроса", reqID)
 	if err := json.NewDecoder(r.Body).Decode(&urlIDs); err != nil {
+		log.Printf("reqID=%s Произошла ошибка при парсинге запроса %v", reqID, err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	log.Printf("reqID=%s ID получены, их количество %d", reqID, len(urlIDs))
 
+	log.Printf("reqID=%s Получаем sign из куки", reqID)
 	sign, err := r.Cookie("sign")
 	if err != nil {
+		log.Printf("reqID=%s Кука не найдена %v", reqID, err)
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 	// work with cookie
+	log.Printf("reqID=%s Начинаем сравнивать подписи", reqID)
 	signID, err = storage.CompareSign(sign.Value, app.Config.Secret)
 	if err != nil {
-		log.Println(err)
+		log.Printf("reqID=%s Подписи не одинаковые, удаление невозможно, %v", reqID, err)
 		signID = 0
 	}
 	// usr not auth
@@ -44,13 +55,21 @@ func (h *Handler) HandleDeleteBatchUrls(w http.ResponseWriter, r *http.Request) 
 	}
 
 	// main
+	log.Printf("reqID=%s Получаем размера буфера", reqID)
 	batchSize = len(urlIDs)
-	inputCh := make(chan dataForWorker, batchSize)
+	log.Printf("reqID=%s Буфер установлен на %d", reqID, batchSize)
 
+	log.Printf("reqID=%s Создаем канал с буфером %d", reqID, batchSize)
+	inputCh := make(chan dataForWorker, batchSize)
+	log.Printf("reqID=%s Канал с буфером %d создан!", reqID, batchSize)
+
+	log.Printf("reqID=%s Складируем в канал ID", reqID)
 	go putJobs(inputCh, urlIDs, signID)
 
+	log.Printf("reqID=%s Запускаем рутину на удаление", reqID)
 	go h.worker(inputCh) // init go routine
 
+	log.Printf("reqID=%s Отдаем ответ со статусом 202", reqID)
 	w.WriteHeader(http.StatusAccepted)
 }
 
@@ -65,26 +84,35 @@ func putJobs(inputCh chan<- dataForWorker, urlIDs []string, signID uint32) {
 }
 
 func getFilledChan(inputCh <-chan dataForWorker, size int) <-chan dataForWorker {
+	log.Printf("Создаем канал с структурой dataForWorker и буфером %v", size)
 	resultCh := make(chan dataForWorker, size)
 	for i := 0; i < size; i++ {
 		job, ok := <-inputCh
 		log.Printf("Chan contains, %v", job)
 		if !ok {
+			log.Printf("Произошла ОШИБКА при чтении данных из канала, выход из цикла")
 			break
 		}
 		resultCh <- job
 	}
+	log.Printf("Закрываем канал")
 	close(resultCh)
+	log.Printf("Возвращаем последние данные %v", resultCh)
 	return resultCh
 }
 
 func (h *Handler) worker(inputCh <-chan dataForWorker) {
+	log.Printf("Воркер запущен!")
 	for {
+		log.Printf("Получаем заполненный канал")
 		filledChan := getFilledChan(inputCh, batchSize)
+		log.Printf("генерируем батч")
 		batches := map[uint32][]string{}
+		log.Printf("Наполняем джобу")
 		for job := range filledChan {
 			batches[job.SignID] = append(batches[job.SignID], job.ID)
 		}
+		log.Printf("НОтправляем данные в БД!")
 		for signID, ids := range batches {
 			err := h.writeToDB(ids, signID)
 			if err != nil {
