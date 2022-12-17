1041d6940e9342436a5af3206c3689593864bc5c
Bauner <bauner@yandex.ru>
2022-12-17 11:46:20 +0300

    add sleep

--- internal/app/handlers/delete_batch.go
+++ internal/app/handlers/delete_batch.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/BawNer/go-shortener-tpl/internal/app"
 	"github.com/BawNer/go-shortener-tpl/internal/app/storage"
@@ -71,6 +72,8 @@ func (h *Handler) HandleDeleteBatchUrls(w http.ResponseWriter, r *http.Request) 
 
 	log.Printf("reqID=%s Отдаем ответ со статусом 202", reqID)
 	w.WriteHeader(http.StatusAccepted)
+
+	time.Sleep(time.Second) // нужно ожидать корректно
 }
 
 func putJobs(inputCh chan<- dataForWorker, urlIDs []string, signID uint32) {
